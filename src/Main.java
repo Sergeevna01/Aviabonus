5720f153374a6801ceac80f746d436005f6d31d6
Sergeevna <l.shliak@docsinbox.ru>
2022-06-21 21:07:10 +0300

    Programm

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 1000;
+        int unit = 20;
+        int result;
+
+        result = cost/unit;
+        System.out.println(result);
+    }
+}
